3e02e227bfa57ed8dd0c8d1704fc7156b4fb5364
Chase Hamby <c.chase.hamby@gmail.com>
2018-10-23 17:46:18 -0500

    get requests in db

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,1 +1,27 @@
-console.log('hi');
+import {writeMovie} from "../javascripts/components/movieComponent.js"
+import {writeMovieDos} from "../javascripts/components/locationComponent.js"
+
+$.get('../db/movie.json')
+  .done((data) => {
+    console.log(data);
+    writeMovie(data.movie);
+  })
+  .fail((error) => {
+    console.error(error);
+  });
+
+// Location below //
+
+const printLocation = () => {
+  $.get('../db/locations.json')
+  .done((data) => {
+    console.log(data);
+    writeMovieDos(data.locations);
+  })
+  .fail((error) => {
+    console.error(error);
+  });
+};
+
+printLocation();
+
