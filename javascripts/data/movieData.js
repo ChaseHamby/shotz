c0890ce8dcc086e86dfa5fd6494582609bc71ca0
Chase Hamby <c.chase.hamby@gmail.com>
2018-10-29 09:44:29 -0500

    working on promises for single movie page div

--- javascripts/data/movieData.js
+++ javascripts/data/movieData.js
@@ -12,35 +12,34 @@ const printMovie = () => {
     });
 };
 
+const moviesWithLocations = (movie) => {
+    return new Promise((resolve, reject)=> {
+        $.get('../db/locations.json')
+            .done((data) => {
+                const moviePins = movie.map(movie1 => {
+                    const matchingLocations = data.locations.filter(location =>location.Movie == movie1.Name);
+                    movie1.locations = matchingLocations;
+                    return movie1
+                })
+                resolve(moviePins);
+      })
+      .fail((error) => {
+        console.error(error);
+        reject(error)
+      }); 
+      })
+};
 
-// const loadMoviesWithLocations = (locationId) => {
-//     return new Promise((resolve, reject) => {
-//       $.get('../db/movie.json')
-//         .done((data) => {
-//           const moviesForPage = data.movies.filter(movie => movie.location_id == locationId)
-//           resolve(moviesForPage);
-//         })
-//         .fail((error) => {
-//           reject(error);
-//         })
-//     })
-//   }
+const moviePromise = () => {
+    printMovie().then((data) => {
+        return moviesWithLocations(data.movie)
+    })
+    .then((moviePins) => {
+        writeMovie(moviePins)
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+};
 
-// const loadMoviesOnLocations = (locations) => {
-//     return new Promise((resolve, reject) => {
-//         $.get('../db/movie.json')
-//         .done((data) => {
-//             const locationsWithMovies = .locations.map(location => {
-//             const matchingMovies = data.movies.filter(movie => movie.location_id === locationId);
-//             location.movies = matchingMovies;
-//             return location;
-//         })
-//             resolve(locationsWithMovies);
-//         })
-//         .fail((error) => {
-//             reject('error loadMoviesOnLocations', error);
-//         })
-//     })
-// }
-
-export {printMovie}
+export {printMovie, moviesWithLocations, moviePromise}
